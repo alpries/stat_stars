472ca6acb92a2f8f7ea20dbd8d090f5ccfbdffac
Bayjaj <rpmoody321@gmail.com>
2025-04-02 01:59:15 -0400

    +Created stats_stars_mockup and example_tag so everyone can be on the same page for the project [we had nothing before.] +Created a sharable Brawl Stars API account and shared it in our gc +Created ALL Proper UI and activities:    If you reference the stats_stars_mockup and example_tag in drawable that I designed in my last commit,    -->StatsStarsActivity & .xml is Activity 1 (Welcome and enter tag screen)    -->PlayerOverview & .xml is Activity 2 (Broad player stats based on given tag)    -->BrawlersActivity & .xml is Activity 3 (List with each individual brawler stats)    -->Settings & .xml is Activity 4 (Settings screen with minimal stuff)    Across all of these, I created plain texts, edit texts, image views, spinners, list views, and did the pretty work like shadows, coloring, font sizing/colors, etc. etc. +Created a Background Gradient to use across all activities [Could this count as our extra implementation?] +Left helpful notes for project guidance

---
+++ app/src/main/java/com/example/stat_stars/StatsStarsActivity.java
@@ -0,0 +1,44 @@
+package com.example.stat_stars;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+// Activity for the initial page
+public class StatsStarsActivity extends AppCompatActivity {
+
+    // VARIABLES + EXPLANATIONS
+    EditText etPlayerTag; // user Player Tag (Example Tag: #UPOQU9U2) Input
+    Button btnGo; // Go button for once the user has entered player tag
+    Button btnBrawlers; // Button that takes them to the brawl stars app (using intents!)
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_statsstars);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        // Drawables
+        etPlayerTag = findViewById(R.id.etPlayerTag); // MAKE SURE THIS CAPS AT 9 OR WHATEVER MAX TAG LENGTH IS
+        btnGo = findViewById(R.id.btnGo);
+        btnBrawlers = findViewById(R.id.btnBrawlers);
+
+
+
+
+        // HELPFUL NOTE: USE THE PUZZLE ACTIVITY TO REMEMBER INTENTS -->
+        // Intent intent = new Intent(MainActivity.this, GameActivity.class);
+        // startActivity(intent);
+    }
+}
--- app/src/main/java/com/example/stat_stars/iSettingsActivity.java
+++ app/src/main/java/com/example/stat_stars/iSettingsActivity.java
@@ -1,6 +1,7 @@
 package com.example.stat_stars;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,23 +9,28 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+// Activity for the Settings page
+public class iSettingsActivity extends AppCompatActivity {
+
+    // VARIABLES + EXPLANATIONS
+    Button btnBack2; // Takes you back to the PREVIOUS activity user was just on
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_isettings);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
+        // Drawables
+        btnBack2 = findViewById(R.id.btnBack2);
+
+
+        // I Don't know what we want to include on the settings page but we need 4 activities so try to come up with something
+        // Also for the actual settings icon, use a SetOnClickListener to make this work!
     }
 }
-
-//should be a simple page, though we can decorate it. i don't remember if it was mobile or web tech
-//when he was talking about copyright so ig just be careful with that?
-
-// Example Tag: #UPOQU9U2
